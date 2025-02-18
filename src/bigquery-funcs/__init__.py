8c005ba63044678ef4db5b07fe4079cba5902628
Alexander Socarras <alex@Alexanders-Mac-mini.local>
2025-02-18 10:00:28 -0500

    remove unused imports in __init__.py

--- src/bigquery-funcs/__init__.py
+++
@@ -1,8 +0,0 @@
-import datetime as dt
-import logging
-from typing import Mapping, Optional, Callable
-
-from google.cloud import bigquery as bq
-from google.cloud.bigquery import SchemaField
-from google.cloud.bigquery.job import _AsyncJob
-
